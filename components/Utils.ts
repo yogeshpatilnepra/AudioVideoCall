9bf0cdd9f4162dae63b16b17cea677576ca42b5f
yogeshpatilnepra <yogesh.patil@nepra.co.in>
2025-02-25 19:34:56 +0530

    utils change.

--- components/Utils.ts
+++ components/Utils.ts
@@ -4,13 +4,12 @@ export default class Utils {
     static async getStream() {
         let isFront = true;
         const sourceInfos = await mediaDevices.enumerateDevices()
-        console.log(sourceInfos);
+            console.log(sourceInfos);
         let videoSourceId;
         for (let i = 0; i < sourceInfos.length; i++) {
             const sourceInfo = sourceInfos[i];
-            if (sourceInfo.kind == "videoinput" && 
-                sourceInfo.facing == (isFront ? "front" : "environment")) 
-                {
+            if (sourceInfo.kind == "videoinput" &&
+                sourceInfo.facing == (isFront ? "front" : "environment")) {
                 videoSourceId = sourceInfo.deviceId;
             }
         }
